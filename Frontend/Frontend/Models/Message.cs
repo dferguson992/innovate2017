3422e86461bad1f7b6de4770c659feb080105c19
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2017-10-03 10:02:19 -0400

    Added Dockerfile and Message object

---
+++ Frontend/Frontend/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Models
+{
+    public class Message
+    {
+        public string Id { get; set; }
+
+        public string Body { get; set; }
+
+        public string Author { get; set; }
+
+        public string Originallink { get; set; }
+    }
+}
